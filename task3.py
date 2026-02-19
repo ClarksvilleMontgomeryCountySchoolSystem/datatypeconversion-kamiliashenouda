b32f0d3ff646720591074a8d5c1d8ad9b9d2daed
kamiliashenouda <kamilia.shenouda653@students.cmcss.net>
2026-02-19 13:09:35 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,25 @@
+from data import a, b, c, d, e, f
 
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+print(a)
+print(type(a))
+a = float(a)
+# b
+print(b)
+print(type(b))
+
+# c
+# TODO 3
+
+# d
+# TODO 4
+
+# e
+# TODO 5
+
+# f
+# TODO 6
